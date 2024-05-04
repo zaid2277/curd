c0967a1e55954ba1c80b6794917fcd631b00c376
zdzd222 <61079710+zdzd222@users.noreply.github.com>
2024-05-04 23:11:42 +0300

    curd

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.master')
+
+@section('title')
+  prodacts
+@stop
+
+@section('page-header')
+    <!-- breadcrumb -->
+@section('PageTitle')
+    products
+@stop
+<!-- breadcrumb -->
+@endsection
+@section('content')
+    <!-- row -->
+    <div class="row">
+        <div class="col-md-12 mb-30">
+            <div class="card card-statistics h-100">
+                <div class="card-body">
+                    <div class="col-xl-12 mb-30">
+                        <div class="card card-statistics h-100">
+                            <div class="card-body">
+                                <a href="{{route('products.create')}}" class="btn btn-success btn-sm" role="button"
+                                   aria-pressed="true">add product</a><br><br>
+                                <div class="table-responsive">
+                                    <table id="datatable" class="table  table-hover table-sm table-bordered p-0"
+                                           data-page-length="50"
+                                           style="text-align: center">
+                                        <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>product</th>
+                                            <th>descrp</th>
+                                            <th>#</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($products as $product)
+                                            <tr>
+                                            <td>{{ $loop->index+1 }}</td>
+                                            <td>{{$product->product}}</td>
+                                            <td>{{$product->descrp}}</td>
+                                            <tr>
+                                                <td>
+                                                    <div class="dropdown show">
+                                                        <a class="btn btn-success btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            pr
+                                                        </a>
+                                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                                            <a class="dropdown-item" href="{{route('products.show',$produc->id)}}"><i style="color: #ffc107" class="far fa-eye "></i>&nbsp;  عرض بيانات الطالب</a>
+                                                            <a class="dropdown-item" href="{{route('products.edit',$produc->id)}}"><i style="color:green" class="fa fa-edit"></i>&nbsp;  تعديل بيانات الطالب</a>
+                                                            <a class="dropdown-item" data-target="#Delete_Student{{ $produc->id }}" data-toggle="modal" href="##Delete_Student{{ $student->id }}"><i style="color: red" class="fa fa-trash"></i>&nbsp;  حذف بيانات الطالب</a>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @include('pages.products.Delete')
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- row closed -->
+@endsection
+@section('js')
+    @toastr_js
+    @toastr_render
+@endsection
